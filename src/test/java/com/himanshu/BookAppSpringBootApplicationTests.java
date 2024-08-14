38c29d7864d64b2919c82a526aaa9770b8cc0262
unknown <hs1572000@gmail.com>
2024-08-14 22:55:34 +0530

    adding my project

---
+++ src/test/java/com/himanshu/BookAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himanshu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
